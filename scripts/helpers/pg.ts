4e6175f034b87aeca38a653781d9035febcfdd08
Tej Birring <tejbirring@gmail.com>
2025-07-02 22:30:32 +0100

    Declare  helper function.

--- scripts/helpers/pg.ts
+++ scripts/helpers/pg.ts
@@ -1,6 +1,22 @@
-import { PoolClient } from "pg";
+import { PoolClient, Client } from "pg";
+import pg from "../../libs/pg";
 
 export const dbExists = async (client: PoolClient, dbName: string) => ((await client.query(`SELECT FROM pg_database WHERE datname='${dbName}'`)).rowCount ?? 0) > 0;
 export const userExists = async (client: PoolClient, userName: string) => ((await client.query(`SELECT FROM pg_roles WHERE rolname='${userName}' AND rolcanlogin='true'`)).rowCount ?? 0) > 0;
 export const roleExists = async (client: PoolClient, userName: string) => ((await client.query(`SELECT FROM pg_roles WHERE rolname='${userName}' AND rolcanlogin='false'`)).rowCount ?? 0) > 0;
 export const countRows = async (client: PoolClient, schemaName: string, tableName: string) => parseInt(((await client.query(`SELECT COUNT(*) FROM "${schemaName}"."${tableName}"`)).rows[0].count));
+
+export const assertTableIsEmpty = async (dbName: string, schemaName: string, tableName: string) => {
+    const client = pg("reader", dbName);
+    try {
+        const nRowsInTable = (await client.query(`SELECT COUNT(*) FROM "${schemaName}"."${tableName}"`)).rows[0].count;
+        if (nRowsInTable > 0) {
+            console.error(`The table must be empty before migration: "${dbName}"."${schemaName}"."${tableName}"`);
+            process.exit(1);
+        }
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+
+}
