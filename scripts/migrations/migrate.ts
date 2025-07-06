6cb21210a69d2119dcc8b674604d388670037c38
Tej Birring <tejbirring@gmail.com>
2025-07-06 01:47:50 +0100

    Refresh lookup table (materialised view) after data migration sequence completion — wrapped in tick/tock profiling.

--- scripts/migrations/migrate.ts
+++ scripts/migrations/migrate.ts
@@ -1,4 +1,7 @@
+import { dbName } from "@/libs/config";
+import pgClient from "@/libs/pg-client";
 import { executeProcessCsvSequence } from "@/libs/process-csv";
+import { withTickTockAsync } from "@/libs/tick-tock";
 
 await executeProcessCsvSequence([
   "scripts/migrations/uk-census-data/migrate-output-areas",
@@ -10,4 +13,15 @@ await executeProcessCsvSequence([
   "scripts/migrations/uk-census-data/migrate-socio-economic-classifications",
 ]);
 
-console.debug("Done!");
+await withTickTockAsync(async () => {
+  console.log("Updating lookup table...");
+  const client = pgClient("editor", dbName);
+  await client.query(
+    `REFRESH MATERIALIZED VIEW "uk-census-data"."output-area-to-postal-codes-lookup"`,
+  );
+  await client.close();
+}).then((duration) => {
+  console.log(`Lookup table updated in ${duration.inSeconds} seconds.`);
+});
+
+console.log("Done!");
